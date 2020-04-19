5c68eece27847a2bfe95dced983e3fa99fe6a24a
MRW Neundorf <ninevillage+naxmefy@gmail.com>
2020-04-19 17:47:09 +0200

    added ui method to RisenAuctionsUITabSpeedSell

--- Modules/RisenAuctionsUITabSpeedSell/Core.lua
+++ Modules/RisenAuctionsUITabSpeedSell/Core.lua
@@ -1,6 +1,15 @@
 -- @type RisenAuctions
 local RisenAuctions = unpack(select(2, ...));
 
+-- @type RisenAuctionsAuctionHouseUI
+local AuctionHouseUI = RisenAuctions:GetModule('RisenAuctionsAuctionHouseUI');
+
+-- locales
+local L = LibStub('AceLocale-3.0'):GetLocale('RisenAuctions');
+
+-- constants
+local DisplayMode = {'RisenAuctionsUITabSpeedSellDisplayMode'}
+
 -- @class RisenAuctionsUITabSpeedSell
 local Mod = RisenAuctions:NewModule('RisenAuctionsUITabSpeedSell',
                                     'AceEvent-3.0', 'AceTimer-3.0');
@@ -30,3 +39,29 @@ function Mod:OnHide()
     self:UnregisterEvent('COMMODITY_SEARCH_RESULTS_UPDATED');
     self:UnregisterEvent('ITEM_SEARCH_RESULTS_UPDATED');
 end
+
+function Mod:UIAddAuctionHouseTab()
+    if self.tabAdded then return; end
+
+    AuctionHouseFrameDisplayMode.RisenAuctionsUITabSpeedSellDisplayMode =
+        DisplayMode;
+    self.tab = AuctionHouseUI:AddAuctionHouseTab(L['Speed Sell'],
+                                                 L['RisenAuctions - Speed Sell'],
+                                                 self, DisplayMode);
+
+    self.tab:SetScript('OnShow', function() Mod:OnShow(); end);
+
+    self.tab:SetScript('OnHide', function() Mod:OnHide(); end);
+
+    self.tabAdded = true;
+
+    self:UIDrawItemsMasterFrame();
+    self:UIDrawItemsDetailFrame();
+    self:UIToggleTabControls(false);
+end
+
+function Mod:UIDrawItemsMasterFrame() end
+
+function Mod:UIDrawItemsDetailFrame() end
+
+function Mod:UIToggleTabControls(state) end
