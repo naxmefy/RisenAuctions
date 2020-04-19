a45f2a1baecb061572212baf09e7e677b3d17bfb
MRW Neundorf <ninevillage+naxmefy@gmail.com>
2020-04-19 17:41:13 +0200

    update l10n for RisenAuctionsUITabSpeedSell

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -1,1 +1,5 @@
 local L = LibStub('AceLocale-3.0'):NewLocale('RisenAuctions', 'deDE', true)
+
+-- RisenAuctionsUITabSell
+L['Speed Seel'] = 'Schnell Verkaufen'
+L['RisenAuctions - Speed Seel'] = 'RisenAuctions - Schnell Verkaufen'
--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,2 +1,5 @@
 local L = LibStub('AceLocale-3.0'):NewLocale('RisenAuctions', 'enUS', true)
 
+-- RisenAuctionsUITabSell
+L['Speed Sell'] = true;
+L['RisenAuctions - Speed Sell'] = true;
